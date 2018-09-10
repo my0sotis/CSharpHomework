1f46cb96f9d0e66121d99782dfe857686b1aff7e
my0sotis <1274034280@qq.com>
2018-09-10 13:22:49 +0800

    第一次作业

---
+++ Homework1/Program1/Program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数字：");
+            string s1 = Console.ReadLine();
+            int a = int.Parse(s1);
+            Console.Write("请输入第二个数字：");
+            string s2 = Console.ReadLine();
+            int b = int.Parse(s2);
+            Console.WriteLine("这两个数的积为{0}。", a * b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework1/Program2/Program2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1 != null&&textBox2 != null)
+            {
+                string s1 = textBox1.Text;
+                int a = int.Parse(s1);
+                string s2 = textBox2.Text;
+                int b = int.Parse(s2);
+                label3.Text = "相乘后的结果为" + a * b;
+            }
+        }
+    }
+}
